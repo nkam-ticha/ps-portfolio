915078652e8ebe85645e4154ed6b46e182b09345
nkam-ticha <128480466+nkam-ticha@users.noreply.github.com>
2023-09-22 11:35:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSkills() {
+  document.querySelector('.shown-content').innerHTML = document.getElementById('skills-id').innerHTML;
+}
+
+function showExperience() {
+  document.querySelector('.shown-content').innerHTML = document.getElementById('experience-id').innerHTML;
+}
+
+function showEducation() {
+  document.querySelector('.shown-content').innerHTML = document.getElementById('education-id').innerHTML;
+}
